3acde8693ae8a7348f4b9bdc94d34709fc89a8a8
e-Ling.Tang <e-ling.tang@geely.com>
2022-03-03 13:12:53 +0800

    feat  物品选中；

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,24 @@ import Stats from 'stats.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
+import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass'
+import { FXAAShader  } from 'three/examples/jsm/shaders/FXAAShader'
 import TWEEN from '@tweenjs/tween.js';
 import { BoxGeometry } from 'three';
 
 const { Scene, Vector3, SpriteMaterial, Sprite, PerspectiveCamera, PlaneGeometry, MeshPhongMaterial, WebGLRenderer, AxesHelper, Color, TextureLoader, MeshBasicMaterial, SphereGeometry, Mesh } = THREE
 
+// 创建一个EffectComposer（效果组合器）对象，然后在该对象上添加后期处理通道。
+let composer: any, renderPass, outlinePass: any, effectFXAA, selectedObjects: any[]
+const mouse = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
 const left = document.getElementById('left')
 const right = document.getElementById('right')
+const a = left?.clientWidth || 100
+const b = left?.clientHeight || 100
 const camera = new PerspectiveCamera( 45, (left?.clientWidth || 100) / (left?.clientHeight || 100), 1, 1000 );
 const camera2 = new PerspectiveCamera( 45, (right?.clientWidth || 100) / (right?.clientHeight || 100), 1, 1000 );
 function aaa(){
@@ -118,7 +129,50 @@ function aaa(){
 
     loadCubeTexture()
 
+    composer = new EffectComposer( renderer );
+    const renderPass = new RenderPass( scene, camera );
+    composer.addPass( renderPass );
+
+    outlinePass = new OutlinePass( new THREE.Vector2( a, b ), scene, camera );
+    // outlinePass.selectedObjects = selectedObjects
+    outlinePass.edgeStrength = 10.0 // 边框的亮度
+    outlinePass.edgeGlow = 0.4// 光晕[0,1]
+    outlinePass.usePatternTexture = false // 是否使用父级的材质
+    outlinePass.edgeThickness = 0.3 // 边框宽度
+    outlinePass.downSampleRatio = 1 // 边框弯曲度
+    outlinePass.pulsePeriod = 5 // 呼吸闪烁的速度
+    outlinePass.visibleEdgeColor.set(new THREE.Color(0x00ff00)) // 呼吸显示的颜色
+    outlinePass.hiddenEdgeColor = new THREE.Color(0, 0, 0) // 呼吸消失的颜色
+    composer.addPass( outlinePass );
+
+    effectFXAA = new ShaderPass( FXAAShader );
+    effectFXAA.uniforms[ 'resolution' ].value.set( 1 / a, 1 / b );
+    composer.addPass( effectFXAA );
+
+    renderer.domElement.style.touchAction = 'none';
+    renderer.domElement.addEventListener( 'pointermove', onPointerMove );
     window.addEventListener( 'resize', onWindowResize );
+  }
+  
+  function onPointerMove( event: any ) {
+    if ( event.isPrimary === false ) return;
+    
+    mouse.x = ( event.offsetX / a ) * 2 - 1;
+    mouse.y = - ( event.offsetY / b ) * 2 + 1;
+    console.log(mouse)
+    checkIntersection();
+  }
+
+  function checkIntersection() {
+    raycaster.setFromCamera( mouse, camera );
+    const intersects = raycaster.intersectObject( scene, true );
+    if ( intersects.length > 0 ) {
+      selectedObjects = [];
+      selectedObjects.push( intersects[ 0 ].object );
+      outlinePass.selectedObjects = selectedObjects;
+    } else {
+      outlinePass.selectedObjects = [];
+    }
   }
 
   // 加载天空环境贴图
@@ -134,7 +188,8 @@ function aaa(){
 
   function animate(t: number) {
       stats.begin();
-      renderer.render( scene, camera );
+      // renderer.render( scene, camera );
+      composer.render();
       requestAnimationFrame( animate );
       group2.rotation.y += 0.01
       group.rotateX(0.02)
